cdd5f5a941f8a8bef1d3f16d80b13bfa5d9e5647
k-bachilo <79043013+k-bachilo@users.noreply.github.com>
2024-03-11 17:09:01 +0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -3,15 +3,18 @@ alias pd="pnpm add -D"
 alias pg="pnpm add -g"
 alias pi="pnpm init"
 alias ps="pnpm start"
+alias pi="pnpm install"
 alias px="pnpx"
 
-alias nd="nodemon"
+alias n="nodemon"
 
 alias gi="go mod init"
 alias gg="go get"
 
 alias h="http"
+alias hd="http DELETE"
 alias hp="http POST"
+alias ht="http PATCH"
 
 # pnpm
 set -gx PNPM_HOME "/Volumes/Storage/Dependencies/pnpm"
